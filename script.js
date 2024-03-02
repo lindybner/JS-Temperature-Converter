ecb440d2b452f36fd886109fbbc915b35bbd7bfa
Jim Lin-Dybner <lin.jycl@gmail.com>
2024-03-02 00:32:48 -0500

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ window.onload = () => {
   const temp = document.getElementById("temp");
   const unit = document.getElementById("unit");
   const btnConvert = document.getElementById("btn-convert");
+  const result = document.getElementById("result");
 
   // EVENT LISTENERS
   btnConvert.addEventListener("click", (event) => {
@@ -12,5 +13,10 @@ window.onload = () => {
     // gather input
     let temperature = parseFloat(temp.value);
     let tempUnit = unit.value;
+
+    // convert calculations if input is in fahrenheit
+    if (tempUnit === "f") {
+      temperature = ((temperature - 32) / 9) * 5;
+    }
   });
 };
