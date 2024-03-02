514430676c00317145a8e6534ee0968aa6f1e4ce
Jim Lin-Dybner <lin.jycl@gmail.com>
2024-03-02 00:26:59 -0500

    convert btn added; event listener added to btn

--- script.js
+++ script.js
@@ -2,4 +2,8 @@ window.onload = () => {
   // VARIABLES
   const temp = document.getElementById("temp");
   const unit = document.getElementById("unit");
+  const btnConvert = document.getElementById("btn-convert");
+
+  // EVENT LISTENERS
+  btnConvert.addEventListener("click", () => {});
 };
